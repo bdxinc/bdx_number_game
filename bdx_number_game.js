f670368dd7b671bd90d646749447b9cd5e2f122b
bdx_inc <bdxinc.contact@gmail.com>
2017-10-10 01:43:53 -0700

    Updated js
    added if/elseif stuff

--- bdx_number_game.js
+++ bdx_number_game.js
@@ -7,13 +7,14 @@ console.log(rNumber);
 
 
 var guesses = 0;
-var returned;
-
+var limit = 3;
+var guess;
 
 function submitGuess() {
+  var returned;
 
-  if (guesses < 3) {
-    var guess = parseInt(document.getElementById("guess").value);
+  if (guesses < limit) {
+    guess = parseInt(document.getElementById("guess").value);
 
     guesses++;
 
@@ -29,7 +30,7 @@ function submitGuess() {
   }
 }
 
-var eL = document.getElementById("submit");
+var eL = document.getElementById("button");
 eL.addEventListener("click", submitGuess, true);
 
 
@@ -39,4 +40,30 @@ function returnGuess(returned) {
   newP.appendChild(text);
 
   document.getElementById("guesses").appendChild(newP);
+
+  if (guess == rNumber) {
+    button.textContent = "PLAY AGAIN";
+  } else if (guesses >= limit){
+    button.textContent = "TRY AGAIN";
+  }
 }
+
+/*
+**************
+vv NONSENSE vv
+**************
+
+**SELECTION**
+
+
+Here is a little breakdown of the flow of the game...
+
+//1. User inputs a number;
+//2. Button is clicked to submit guess;
+/3. Validate user input;
+4. Check guess (has user run out of guesses or guesses the correct number);
+5. Change form display if necessary;
+6. Log guess history;
+7. When the game is over allow user to play again.
+8. Reset game.
+*/
